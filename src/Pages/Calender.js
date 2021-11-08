93f9af8ee0976679ecdd0e8f579ca9a93a2fb26f
aditijain02 <84717818+aditijain02@users.noreply.github.com>
2021-11-08 15:07:02 +0530

    Add files via upload

---
+++ src/Pages/Calender.js
@@ -0,0 +1,9 @@
+import MotionHoc from "./MotionHoc";
+
+const CalenderComponent = () => {
+  return <h1>Register</h1>;
+};
+
+const Calender = MotionHoc(CalenderComponent);
+
+export default Calender;
---
+++ src/Pages/Login.js
@@ -0,0 +1,9 @@
+import MotionHoc from "./MotionHoc";
+
+const Login = () => {
+  return <h1>LogIn</h1>;
+};
+
+const Log = MotionHoc(Login);
+
+export default Login;
